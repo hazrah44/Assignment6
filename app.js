bf4db877d45fe40000086f4787d8e19342139041
hazrahothman <77723723+hazrahothman@users.noreply.github.com>
2021-12-14 03:26:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    username: 'CoderInTraining',
+    newTweet: '',
+    tweets: [
+      'Started learning to code today. Wish me luck!', 
+      'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.', 
+      'Today I start learning Vue. I got this.'
+    ],
+    bio: 'Excited future software engineer.'
   }
-})
+});
